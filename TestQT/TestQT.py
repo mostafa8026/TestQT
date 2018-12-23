2f71e39f6f006a210e0352c0e673384ee74ddbd8
mostafa H <mostafaheidary@hotmail.com>
2018-12-23 09:23:27 +0330

    Just add the main example which is introduces in thsi site: http://doc.qt.io/qtforpython/gettingstarted.html

--- TestQT/TestQT.py
+++ TestQT/TestQT.py
@@ -1,20 +1,33 @@
 import sys
-from PySide2.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+import random
+from PySide2 import QtWidgets, QtCore, QtGui
 
-class MyForm(QWidget):
+class MyForm(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.hellolabel = QLabel('hello world!')
 
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.hellolabel)
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+
+        self.button = QtWidgets.QPushButton("Click me!")
+        self.text = QtWidgets.QLabel("<font color=red size=30>%s</font>"%"Hello World")
+        self.text.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
         self.setLayout(self.layout)
+
+        self.button.clicked.connect(self.magic)
+
+
+    def magic(self):
+        self.text.setText("<font color=red size=30>%s</font>"%random.choice(self.hello))
 
 
 if __name__ == "__main__":
-    app = QApplication()
+    app = QtWidgets.QApplication()
     widget = MyForm()
-    widget.resize(800, 600)
+    widget.resize(400, 300)
     widget.show()
 
     sys.exit(app.exec_())
